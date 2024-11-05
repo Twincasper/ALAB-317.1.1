bbe66bdea260045b7d995046c4cc89ef40bb1e4d
Justin Watson <61891187+Twincasper@users.noreply.github.com>
2024-11-05 18:01:23 -0500

    Refactored index.ts to add helper function

--- src/index.ts
+++ src/index.ts
@@ -51,44 +51,89 @@ myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.model);
 
-// Copy the existing Vehicle class definition as a starting point for NCycle.
-// Modify NCycle to accept a generic type.
-// Allow make and model to have either the generic type or an array of the generic type.
-// Adjust the constructor parameters accordingly.
-// Create a new method print, which returns nothing and has a single number parameter (either optional or defaulted to 0).
-// Use type guards and other appropriate techniques to implement print such that it logs:
-// "This is a <make> <model> NCycle." if make and model are not arrays.
-// "This NCycle has a <make> <model> at <parameter>." if make and model are arrays and the index of parameter exists in each.
-// "This NCycle was not created properly." if neither of the above are true.
 
-class NCycle<T> extends Vehicle {
-  constructor(make: T, model: T, wheels: number) {
-    super(make, model, wheels);
+class NCycle<T> {
+  status: "started" | "stopped" = "stopped";
+  make: T | T[];
+  model: T | T[];
+
+  constructor(make: T | T[], model: T | T[]) {
+    this.make = make;
+    this.model = model;
   }
 
-  print(parameter: number = 0) {
-    if (typeof this.make === "string" && typeof this.model === "string") {
-      if (Array.isArray(this.make) && Array.isArray(this.model)) {
-        if (parameter >= 0 && parameter < this.make.length && parameter < this.model.length) {
-          console.log(`This NCycle has a ${this.make[parameter]} ${this.model[parameter]} at ${parameter}.`);
-        } else {
-          console.log("This NCycle was not created properly.");
-        }
+  start(): void {
+    this.status = "started";
+  }
+
+  stop(): void {
+    this.status = "stopped";
+  }
+
+  // Could probably make an array helper fhcecker for all the array checks
+
+  private isArray(value: T | T[]): value is T[] {
+    return Array.isArray(value);
+  }
+
+  print(index: number = 0): void {
+    if (!this.isArray(this.make) && !this.isArray(this.model)) {
+      console.log(`This is a ${this.make} ${this.model} NCycle.`);
+    } else if (this.isArray(this.make) && this.isArray(this.model)) {
+      if (this.make[index] && this.model[index]) {
+        console.log(`This NCycle has a ${this.make[index]} ${this.model[index]} at ${index}.`);
       } else {
-        console.log(`This is a ${this.make} ${this.model} NCycle.`);
+        console.log("This NCycle was not created properly.");
       }
     } else {
       console.log("This NCycle was not created properly.");
     }
   }
 
-  printAll() {
-    if (Array.isArray(this.make) && Array.isArray(this.model)) {
-      for (let i = 0; i < Math.min(this.make.length, this.model.length); i++) {
-        this.print(i);
+  printAll(): void {
+    if (this.isArray(this.make) && this.isArray(this.model)) {
+      if (this.make.length === this.model.length) {
+        for (let i = 0; i < this.make.length; i++) {
+          console.log(`This NCycle has a ${this.make[i]} ${this.model[i]} at ${i}.`);
+        }
+      } else {
+        console.log("This NCycle was not created properly.");
       }
+    } else if (!this.isArray(this.make) && !this.isArray(this.model)) {
+      console.log(`This is a ${this.make} ${this.model} NCycle.`);
     } else {
       console.log("This NCycle was not created properly.");
     }
   }
 }
+
+const testCycle1 = new NCycle<number>(1, 2, 3);
+testCycle1.print();
+testCycle1.printAll();
+
+const testCycle2 = new NCycle<string>("This", "That", 4);
+testCycle2.print();
+testCycle2.printAll();
+
+const testCycle3 = new NCycle<string>("Make", 10, 4);
+testCycle3.print(4);
+testCycle3.printAll();
+
+const makes4 = ["Volkswagon", "Tesla", "Audi"];
+const models4 = ["Passat", "Model X", "A4"];
+const testCycle4 = new NCycle<string[]>(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+
+const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const models5 = [1, 1, 2, 3, 5];
+const testCycle5 = new NCycle<number[]>(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+add(testCycle1.make, testCycle5.model[1]);
+// Error expected here
+add(testCycle2.make, testCycle4.model[1]);
